1c61b808619b2bc904fdfbe15cc7f1909f33d119
Andrew Kiluk <andrew.kiluk@gmail.com>
2016-04-25 07:53:19 -0700

    Remove cmus

--- .config/cmus/playlist.pl
+++
@@ -1,7 +0,0 @@
-/home/andrew/Music/Parov Stelar/The Princess/CD1/01 - Milla's Dream.mp3
-/home/andrew/Music/Parov Stelar/The Princess/CD1/02 - All Night.mp3
-/home/andrew/Music/Parov Stelar/The Princess/CD1/03 - Nobody's Fool feat. Cleo Panther.mp3
-/home/andrew/Music/Parov Stelar/The Princess/CD1/04 - The Princess.mp3
-/home/andrew/Music/Parov Stelar/The Princess/CD1/05 - Silent Shuffle feat. Jerry Di Monza.mp3
-/home/andrew/Music/Parov Stelar/The Princess/CD1/06 - This Game feat. Anduze.mp3
-/home/andrew/Music/Parov Stelar/The Princess/CD1/07 - You Got Me There.mp3
